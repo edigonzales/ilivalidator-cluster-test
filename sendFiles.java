c37bb72931d94211cc0b48dc48b20bff8a7f0d36
Stefan Ziegler <edi.gonzales@gmail.com>
2022-07-24 07:28:25 +0200

    inital commit

--- sendFiles.java
+++ sendFiles.java
@@ -17,22 +17,12 @@ public class sendFiles {
     public static void main(String... args) throws IOException {
         out.println("Hello World");
 
-        //var path = Paths.get("/Users/stefan/Downloads/av_BE_lv95");
         var directory = "/Users/stefan/Downloads/av_BE_lv95";
         var fileExtension = "ITF";
-
-        /* 
-        if (!Files.isDirectory(path)) {
-            throw new IllegalArgumentException("Path must be a directory!");
-        }
-
-        List<String> files;
-        */
 
         List<Path> pathList = new ArrayList<>();
 
         try (Stream<Path> stream = Files.walk(Paths.get(directory))) {
-              // Do something with the stream.
               pathList = stream.map(Path::normalize)
                 .filter(Files::isRegularFile)
                 .filter(path -> path.getFileName().toString().toUpperCase().endsWith(fileExtension))
