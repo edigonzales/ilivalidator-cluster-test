6c2372fd7dea9dfb99127648bc89ddb9acb9798b
Stefan Ziegler <edi.gonzales@gmail.com>
2022-07-24 11:18:04 +0200

    upload files

--- sendFiles.java
+++ sendFiles.java
@@ -1,9 +1,10 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-
+//DEPS org.apache.httpcomponents.client5:httpclient5:5.1.3 
 
 import static java.lang.System.*;
 
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,13 +13,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.mime.FileBody;
+import org.apache.hc.client5.http.entity.mime.HttpMultipartMode;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.ProtocolException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
 public class sendFiles {
 
-    public static void main(String... args) throws IOException {
+    public static void main(String... args) throws IOException, ParseException {
         out.println("Hello World");
 
         var directory = "/Users/stefan/Downloads/av_BE_lv95";
         var fileExtension = "ITF";
+        var uploadUrl = "http://localhost:8080/rest/jobs";
 
         List<Path> pathList = new ArrayList<>();
 
@@ -29,5 +44,37 @@ public class sendFiles {
                 .collect(Collectors.toList());
         }
         pathList.forEach(System.out::println);
+
+        var httpClient = HttpClients.createDefault();
+
+        for (Path path : pathList) {
+            var httpPost = new HttpPost(uploadUrl);
+            var fileBody = new FileBody(path.toFile(), ContentType.DEFAULT_BINARY);
+    
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setMode(HttpMultipartMode.STRICT);
+            builder.addPart("file", fileBody);
+            HttpEntity entity = builder.build();
+
+            httpPost.setEntity(entity);
+            var response = httpClient.execute(httpPost);
+
+            out.println(response.getCode());
+            try {
+                out.println(response.getHeader("Operation-Location"));
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+
+   
+      
+            break;
+        }
+
+
+
+
+
+      
     }
 }
